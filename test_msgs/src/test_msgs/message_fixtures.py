743cb7e302b9c46a17ffabd5df2484757939b1a7
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-06-24 14:07:37 -0700

    use a BOM for the message fixtures Python file
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- test_msgs/src/test_msgs/message_fixtures.py
+++ test_msgs/src/test_msgs/message_fixtures.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Open Source Robotics Foundation, Inc.
+﻿# Copyright 2016 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# This file starts with a BOM since it contain non-ASCII characters
 
 from test_msgs.msg import Arrays
 from test_msgs.msg import BasicTypes
